4278f58a65a3befef94c43c859e60eb075b23bb6
phenchanter <phenchanter@gmail.com>
2014-08-08 11:09:11 +0400

    java

--- src/android/EstimoteBeacons.java
+++ src/android/EstimoteBeacons.java
@@ -135,7 +135,7 @@ public class EstimoteBeacons extends CordovaPlugin {
             public void onBeaconsDiscovered(Region region, List<Beacon> beacons) {
                 if(beacons == null || beacons.isEmpty()) {
                     Log.d("DEBUG", "No beacons");
-                    if(EstimoteBeacons.this.beacons.length>0) {
+                    if(EstimoteBeacons.this.beacons.size()>0) {
                         EstimoteBeacons.this.beacons.clear();
                     }
                 } else {
